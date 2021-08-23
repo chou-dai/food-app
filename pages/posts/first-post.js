d3d4c1645c31b38908eb5f9f8e94defe84419fc4
chou-dai <c.daiki@gmail.com>
2021-08-24 00:02:07 +0900

    test1

---
+++ pages/posts/first-post.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+
+function FirstPost() {
+  return (
+    <>
+      <h1>
+        food kappa
+      </h1>
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </>
+  )
+}
+
+export default FirstPost
