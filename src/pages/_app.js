130961a082c4d404fc26568d2d37cc0bbdb8a92d
chou-dai <c.daiki@gmail.com>
2021-08-25 01:11:02 +0900

    test8

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
-import '../assets/style.css'
-import 'tailwindcss/tailwind.css'
+import '../styles/style.css'
+// import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
