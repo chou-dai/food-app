19bcf800be87fb541d98707ea43e0f98505f126b
chou-dai <c.daiki@gmail.com>
2021-09-08 19:23:16 +0900

    test45

--- src/components/Menus/MenuSearch.js
+++ src/components/Menus/MenuSearch.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import SortIcon from '@material-ui/icons/Sort';
 import SearchIcon from '@material-ui/icons/Search';
 
 const MenuSearch = () => {
-  const search = (e) => {
+  const [searchText, setSearchText] = useState("");
+
+  const inpuText = useCallback((event) => {
+    setSearchText(event.target.value)
+  }, [setSearchText]);
+
+  const searchSubmit = (e) => {
+    if(searchText === "") return;
     if(e.key === "Enter") {
-      window.alert("検索処理")
+      window.alert(`入力テキスト：${searchText}\n検索処理`)
     }
+  }
+
+  const sort = () => {
+    window.alert("並び替え処理")
   }
 
   return (
     <div className="flex w-full bg-gray-100 px-3 py-2 sticky-position z-20">
-      <div className="flex border-2 rounded w-full pl-2">
-        <SearchIcon className="text-gray-500 bg-white h-full" />
-        <input type="serch" className="appearance-none px-2 py-1 w-full focus:outline-none" onKeyPress={search} placeholder="検索" />
+      <div className="flex bg-white border-2 rounded w-full pl-2">
+        <SearchIcon className="text-gray-500 h-full" />
+        <input
+          type="serch" className="appearance-none px-2 py-1 w-full focus:outline-none" placeholder="検索"
+          value={searchText} onChange={inpuText} onKeyPress={searchSubmit} />
       </div>
-      <button className="border-2 items-center justify-center px-2 bg-white">
+      <button className="border-2 items-center justify-center px-2 bg-white" onClick={sort}>
         <SortIcon className=" text-gray-500" />
       </button>
     </div>
--- src/components/Restaurants/RestTopImage.js
+++ src/components/Restaurants/RestTopImage.js
@@ -9,7 +9,7 @@ import Tooltip from '@material-ui/core/Tooltip';
 const useStyles = makeStyles((theme) => ({
   absolute: {
     position: 'absolute',
-    bottom: theme.spacing(4.8),
+    bottom: theme.spacing(5.5),
     right: theme.spacing(1.3),
   },
 }));
@@ -17,22 +17,27 @@ const useStyles = makeStyles((theme) => ({
 const RestTopImage = (props) => {
   const classes = useStyles();
 
+  const changeImage = () => {
+    window.alert("画像変更処理")
+  }
+
   return (
-    <div className="fixed top-0 content-center h-80 w-full shadow-md bg-white">
+    <div className="fixed top-0 content-center w-full shadow-md bg-white" style={{"height":"20rem"}}>
       <Image
         src={props.image.path}
         className="shadow object-cover"
         style={{"backgroud":"none", "padding":0, "width":"100%", "height":"100%"}}
         loading={<CircularProgress style={{'color': '#9400d3'}} />}
       />
-      <div className="rest-mv-glass w-4/5 h-1/2">
-        <h1 className="text-3xl font-semibold center text-white" style={{"text-shadow":"1px 1px 16px black"}}>{props.name}</h1>
-      </div>
-      <Tooltip title="画像変更" aria-label="change">
+      {/* <div className="absolute top-0 w-full h-72">
+        <div className="rest-mv-glass w-4/5 h-1/2" style={{"border-radius":"10px"}}>
+          <h1 className="text-3xl font-semibold center text-white" style={{"text-shadow":"1px 1px 16px black"}}>{props.name}</h1>
+        </div>
+      </div> */}
+      <Tooltip title="画像変更" aria-label="change" onClick={changeImage}>
         <Fab
           style={{"width":"48px", "height":"48px", "background":"rgba(2,2,2,0.3)", "color":"#e0e0e0"}}
-          className={classes.absolute}
-        >
+          className={classes.absolute} >
           <AddPhotoAlternateOutlinedIcon />
         </Fab>
       </Tooltip>
